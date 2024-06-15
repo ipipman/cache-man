62456fcf0dd2918d4e8f38682faba19a2cbd8a2a
ipipman <ipipman@163.com>
2024-06-15 13:26:09 +0800

    init framework

---
+++ src/main/java/cn/ipman/cache/CacheManApplication.java
@@ -0,0 +1,13 @@
+package cn.ipman.cache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CacheManApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CacheManApplication.class, args);
+    }
+
+}
