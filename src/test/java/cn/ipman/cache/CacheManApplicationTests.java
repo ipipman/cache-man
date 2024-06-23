741fde79b669d2f270a237726a13cb4e54912b8e
ipipman <ipipman@163.com>
2024-06-23 22:18:42 +0800

    close spring test

--- src/test/java/cn/ipman/cache/CacheManApplicationTests.java
+++ src/test/java/cn/ipman/cache/CacheManApplicationTests.java
@@ -3,11 +3,11 @@ package cn.ipman.cache;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class CacheManApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//@SpringBootTest
+//class CacheManApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
